8156f795d3b838875cba99b44ce9f0a7c37e4adc
Rudhrakshdeethi <rudhraksh319@gmail.com>
2025-12-05 09:40:59 +0530

    first commit

---
+++ spinny/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterView } from "vue-router";
+import herosection from "./components/herosection.vue";
+</script>
+
+<template class="min-h-screen bg-[#F8F8F8]">
+  <RouterView />
+</template>
---
+++ spinny/src/pages/Carspage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Navbar from "../components/Navbar.vue";
+import ProductsPage from "../components/ProductsPage.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div class="mt-20">
+    <ProductsPage />
+  </div>
+</template>
